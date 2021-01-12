ba2ce9a7efd806cda673b222df72383c0497a9a0
skuttenkuler <sam.kuttenk@gmail.com>
2021-01-12 15:12:56 -0800

    created superuser, admin page working, app migration, env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+#set up read env
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4az9pdupjqq)v^y$a=_ol1al3hogx_f0uj@yncze2aau2=&#rl'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'danarobbins',
 ]
 
 MIDDLEWARE = [
--- backend/danarobbins/models.py
+++ backend/danarobbins/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Gigs(models.Model):
+    venue = models.CharField(max_length=50)
+    time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    artist_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self
+
