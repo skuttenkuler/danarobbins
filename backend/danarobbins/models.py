77a92ef02d38c3aedad3485d20a5b1ab28620661
skuttenkuler <sam.kuttenk@gmail.com>
2021-01-14 21:19:20 -0800

    object added to admin and adding gig objects

--- backend/danarobbins/models.py
+++ backend/danarobbins/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Gigs(models.Model):
-    venue = models.CharField(max_length=50)
-    time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    artist_name = models.CharField(max_length=50)
+    venue = models.CharField(max_length=50, default='')
+    time = models.DateTimeField(auto_now=False, auto_now_add=False, default='')
+    artist_name = models.CharField(max_length=50, default='')
+    ticket_link = models.URLField(("Concert Link"), max_length=200)
 
     def __str__(self):
-        return self
+       return self.artist_name
 
